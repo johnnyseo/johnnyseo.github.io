fee9d1275993ec6a18aa2c6d21ac30953ff6f48f
JohnnySeo <tjwhd6860@naver.com>
2024-02-16 17:59:18 +0900

    [FIX] 반응형적용

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -6,12 +6,13 @@ type PostContentProps = {
 }
 
 const MarkdownRenderer = styled.div`
-  // Renderer style
+  // Renderer Style
   display: flex;
   flex-direction: column;
   width: 768px;
   margin: 0 auto;
   padding: 100px 0;
+  word-break: break-all;
 
   // Markdown Style
   line-height: 1.8;
@@ -30,6 +31,7 @@ const MarkdownRenderer = styled.div`
     font-weight: 800;
     margin-bottom: 30px;
   }
+
   * + h1,
   * + h2,
   * + h3 {
@@ -96,6 +98,34 @@ const MarkdownRenderer = styled.div`
   code[class*='language-'],
   pre[class*='language-'] {
     tab-size: 2;
+  }
+
+  // Markdown Responsive Design
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 80px 20px;
+    line-height: 1.6;
+    font-size: 14px;
+
+    h1 {
+      font-size: 23px;
+    }
+
+    h2 {
+      font-size: 20px;
+    }
+
+    h3 {
+      font-size: 17px;
+    }
+
+    img {
+      width: 100%;
+    }
+
+    hr {
+      margin: 50px 0;
+    }
   }
 `
 
